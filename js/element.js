21d97cd9c6b764bcee5c0b626e28f086ef5b7392
tkachenko-vladislav <tkachenko.vladislav@rambler.ru>
2015-05-14 15:11:57 +0300

    add availability page month table

--- js/element.js
+++ js/element.js
@@ -106,7 +106,7 @@ $(function() {
   // Availability page
 
   // Date elements
-<<<<<<< HEAD
+// <<<<<<< HEAD
  $(function() {
     $( "#from" ).datepicker({
       defaultDate: "+1w",
@@ -126,13 +126,6 @@ $(function() {
 
     });
   });
-  // var dateElements = $(".start-date");
-  // if (dateElements.length) $(".start-date").datepicker();
-=======
-
-  var dateElements = $(".start-date");
-  if (dateElements.length) $(".start-date").datepicker();
->>>>>>> 728312ec5c421dee8e7eb6fa91ecfbfb09e460cb
 
   // Filter activator
 
